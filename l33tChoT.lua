5f5f926a2b341c3a21889b063f57518adeb6f960
N1Vo <farcinkiewicz.dom@gmail.com>
2015-08-15 15:09:08 +0100

    Update l33tChoT.lua

--- l33tChoT.lua
+++ l33tChoT.lua
@@ -33,7 +33,7 @@ if IWalkConfig.Combo then
   if ValidTarget(target, 950) then
   
   local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,1200,GetCastRange(myHero,_Q),250,false,true)
-   local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,250,GetCastRange(myHero,_W),210,false,true)
+   local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,250,GetCastRange(myHero,_Q),210,false,true)
    
    if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
@@ -50,8 +50,13 @@ if IWalkConfig.Harass then
   if ValidTarget(target, 950) then
   
   local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,1200,GetCastRange(myHero,_Q),250,false,true)
-  if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
+  local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,250,GetCastRange(myHero,_W),210,false,true)
+  
+  if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.HarassQ then
 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+end
+if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and Config.HarassW then
+CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 end
 end
 end
